48a35a10f7b9f208f1ea5dc69acd052cb1cb737f
dvberkel <daan.v.berkel.1980@gmail.com>
2015-04-17 13:46:43 +0200

    Create a minimal heroku deployable app
    See https://github.com/heroku/clojure-getting-started for details

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/uberjar/clojurenauts-twitter-bot.jar clojure.main -m bot.web
--- project.clj
+++ project.clj
@@ -3,7 +3,14 @@
   :url "https://github.com/ClojureNauts/clojurenauts-twitter-bot"
   :license {:name "The MIT License (MIT)"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [compojure "1.1.8"]
+                 [ring/ring-jetty-adapter "1.2.2"]
+                 [environ "0.5.0"]]
+  :min-lein-version "2.0.0"
+  :plugins [[environ/environ.lein "0.2.1"]]
+  :hooks [environ.leiningen.hooks]
+  :uberjar-name "clojurenauts-twitter-bot.jar"
+  :profiles {:uberjar {:aot :all} :production {:env {:production true}}}
   :main ^:skip-aot bot.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :target-path "target/%s")
