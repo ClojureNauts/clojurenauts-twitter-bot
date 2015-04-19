0a522c9537ec46a555e0e29a31a402ecbf53757b
dvberkel <daan.v.berkel.1980@gmail.com>
2015-04-19 12:14:33 +0200

    An example profiles.clj
    Make sure that actual values do not end up in version control!
    
    use the command `git update-index --assume-unchanged profiles.clj`

---
+++ profiles.clj
@@ -0,0 +1,6 @@
+;; Make sure that the actual profiles.clj is not put under version control!
+;; Consumer key and secret can be found at https://dev.twitter.com/apps/
+{:dev {:env {:consumer-key "your consumer key"
+             :consumer-secret "your consumer secret"
+             :access-token "your access token"
+             :access-token-secret "your access token secret"}}}
