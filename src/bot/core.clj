613818d06f3c69e2a13f35e29ffe5f0e9e976201
dvberkel <daan.v.berkel.1980@gmail.com>
2015-04-20 16:53:52 +0200

    Let main method start a server

--- src/bot/core.clj
+++ src/bot/core.clj
@@ -1,7 +1,15 @@
 (ns bot.core
+  (:use [bot.web :only [app]]
+        [environ.core :only [env]]
+        [compojure.handler :only [site]])
+  (:require [ring.adapter.jetty :as jetty])
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, Clojureverse!"))
+(defn -main [& [port]]
+  "respond to requests"
+  (let [port (Integer. (or port (env :port) 5000))]
+    (jetty/run-jetty (site #'app) {:port port :join? false})))
+
+;; For interactive development:
+;; (.stop server)
+;; (def server (-main))
--- src/bot/web.clj
+++ src/bot/web.clj
@@ -1,10 +1,7 @@
 (ns bot.web
   (:require [compojure.core :refer [defroutes GET PUT POST DELETE ANY]]
-            [compojure.handler :refer [site]]
             [compojure.route :as route]
-            [clojure.java.io :as io]
-            [ring.adapter.jetty :as jetty]
-            [environ.core :refer [env]]))
+            [clojure.java.io :as io]))
 
 (defn splash []
   {:status 200
@@ -16,11 +13,3 @@
        (splash))
   (ANY "*" []
        (route/not-found (slurp (io/resource "404.html")))))
-
-(defn -main [& [port]]
-  (let [port (Integer. (or port (env :port) 5000))]
-    (jetty/run-jetty (site #'app) {:port port :join? false})))
-
-;; For interactive development:
-;; (.stop server)
-;; (def server (-main))
