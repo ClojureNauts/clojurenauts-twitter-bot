5787c991b73e2ad7d6a564c8fd96f76772480bfd
dvberkel <daan.v.berkel.1980@gmail.com>
2015-04-20 19:50:38 +0200

    Configure logging framework
    Fixes #2

--- project.clj
+++ project.clj
@@ -7,7 +7,10 @@
                  [compojure "1.1.8"]
                  [ring/ring-jetty-adapter "1.2.2"]
                  [environ "0.5.0"]
-                 [twitter-api "0.7.8"]]
+                 [twitter-api "0.7.8"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [org.slf4j/slf4j-log4j12 "1.7.12"]
+                 [log4j/log4j "1.2.17"]]
   :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.2.1"]]
   :hooks [environ.leiningen.hooks]
