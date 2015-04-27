417a034b1e949135225fe99097df9690d4016a7f
dvberkel <daan.v.berkel.1980@gmail.com>
2015-04-27 11:34:28 +0200

    Accept request parameter in POST request

--- src/bot/web.clj
+++ src/bot/web.clj
@@ -1,23 +1,26 @@
 (ns bot.web
   (:require [compojure.core :refer [defroutes GET PUT POST DELETE ANY]]
             [compojure.route :as route]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [clojure.tools.logging :as log]))
 
 (defn splash []
   {:status 200
    :headers {"Content-Type" "text/plain"}
    :body (pr-str ["Hello" :from 'ClojureNauts])})
 
-(defn webhook []
+(defn webhook [request]
   "handles POST request from GitHub webhooks"
-  {:status 200
-   :headers {"Content-Type" "text/plain"}
-   :body "OK"})
+  (do
+    (log/info "Received request" request)
+    {:status 200
+       :headers {"Content-Type" "text/plain"}
+       :body "OK"}))
 
 (defroutes app
   (GET "/" []
        (splash))
-  (POST "/" []
-        (webhook))
+  (POST "/" request
+        (webhook request))
   (ANY "*" []
        (route/not-found (slurp (io/resource "404.html")))))
